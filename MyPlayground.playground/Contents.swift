31ad76b04c638e24cfc1b4c79696c0db9c50f922
Jennifer <jennifer@Jenniferde-MacBook-Air.local>
2017-02-18 17:23:46 +0800

    my homework 5 第一題

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, my homework 5"
+
+var sum = 0
+for i in 0 ... 7
+{
+    for j in 0 ... 7
+    {
+        sum = sum + j * i
+        
+    }
+    
+}
+sum
